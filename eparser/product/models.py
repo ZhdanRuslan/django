880298083fc2bc805f5734c38f684baa545a63e0
ruslan <zhd.ruslan@gmail.com>
2019-02-10 19:34:40 +0200

    add new model

--- eparser/product/models.py
+++ eparser/product/models.py
@@ -7,7 +7,20 @@ class Phone(models.Model):
 
     brand = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
-    year = models.IntegerField(max_length=255)
+    year = models.IntegerField()
+    model = models.CharField(max_length=255)
+    price = models.FloatField(max_length=255)
+
+    def __str__(self):
+        return 'Brand - {} Name - {} Version -  {}, price - {}'.format(self.brand, self.name, self.model, self.price)
+
+class Laptop(models.Model):
+
+    initial = True
+
+    brand = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    year = models.IntegerField()
     model = models.CharField(max_length=255)
     price = models.FloatField(max_length=255)
 
--- eparser/product/views.py
+++ eparser/product/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import Phone
+from .models import Phone, Laptop
 # Create your views here.
 
 
 def index(request):
     context = dict()
     all_phones = Phone.objects.all()
+    all_laptops = Laptop.objects.all()
     context['all_phones'] = all_phones
+    context['all_laptops'] = all_laptops
     return render(request, 'product/index.html', context)
 
 
@@ -14,8 +16,11 @@ def index(request):
 
 def detail_view(request, pk):
     phone = Phone.objects.get(id=pk)
+    laptop = Laptop.objects.get(id=pk)
     ctx = {
-        'phone_instance': phone
+        'phone_instance': phone,
+        'laptop_instance': laptop
+
     }
 
     return render(request, 'product/detail_view.html', ctx)
