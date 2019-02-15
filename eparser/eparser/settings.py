71f3259f825660397d3ae45fb9b17fa04b321697
Ruslan Zhdan <r.zhdan@betinvest.com>
2019-02-15 13:37:11 +0200

    add logout and delete all data

--- eparser/eparser/settings.py
+++ eparser/eparser/settings.py
@@ -130,4 +130,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_URL = 'accounts/login/'
 LOGIN_REDIRECT_URL = '/'
--- eparser/product/views.py
+++ eparser/product/views.py
@@ -1,21 +1,30 @@
+from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from django.views import View
+
 from .models import Vacancy
 from .main import start_app
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class Index(View):
     template_name = 'product/index.html'
     context = dict()
 
+    @method_decorator(login_required, settings.LOGIN_URL)
     def get(self, request):
-
-        if request.user.is_authenticated:
-            if request.GET.get('mybtn') == 'START':
-                start_app()
-            all_vacancies = Vacancy.objects.all()
-            self.context['all_vacancies'] = all_vacancies
-            return render(request, self.template_name, self.context)
-        else:
-            return redirect('accounts/login/')
-
+        if request.GET.get('parse_btn') == 'Parse':
+            start_app()
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        if request.GET.get('logout_btn') == 'Logout':
+            print('OK')
+            logout(request)
+            return redirect(settings.LOGIN_URL)
+        if request.GET.get('delete_btn') == 'Delete':
+            Vacancy.objects.all().delete()
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        all_vacancies = Vacancy.objects.all()
+        self.context['all_vacancies'] = all_vacancies
+        return render(request, self.template_name, self.context)
